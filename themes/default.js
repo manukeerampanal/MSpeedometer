08f360698ee93253be3c74f179cc04c6da2f67ec
Manu Mathew <manukeerampanal@users.noreply.github.com>
2015-07-10 19:48:07 +0530

    Create default.js

---
+++ themes/default.js
@@ -0,0 +1,20 @@
+MSpeedometer.themes['default'] = {
+    dial        : 'Gray',
+    rim         : 'SlateGray',
+    rimArc      : 'Gainsboro',
+    thresh      : 'LawnGreen',
+    center      : 'Black',
+    nose        : 'SlateGray',
+    hand        : 'Black',
+    handShine   : 'SlateGray',
+    handShineTo : 'Black',
+    ticks       : 'Black',
+    marks       : 'Black',
+    strings     : 'Black',
+    digits      : 'Black',
+    font        : 'Sans-Serif',
+    titlecolor  : 'red',
+    titlefont   : 'Sans-Serif',
+    titlesize   : '14pt',
+    titleweight : 'bold'
+};
